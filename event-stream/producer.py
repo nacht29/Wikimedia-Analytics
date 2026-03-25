1f23afcc944819a445cdd4abfa1499f017dae4aa
nacht29 <nacht29.study@gmail.com>
2026-03-25 19:12:19 +0800

    feat: Successfully implemented prototype to write data to Kafka Topic via Python SSE Producer
    - stream data from Wikimedia recentchange EventStream
    - config and create a Kafka Broker instance with Python
    - publish data to Kafka Topic via producer

--- event-stream/producer.py
+++ event-stream/producer.py
@@ -1,22 +1,44 @@
 import json
 from requests_sse import EventSource
+from confluent_kafka import Producer
+import socket
 
-url = 'https://stream.wikimedia.org/v2/stream/recentchange'
-headers = {"User-Agent": "Wikimedia-Analytics/0.1 nacht29.study@gmail.com"}
+# Kafka broker
+BOOTSTRAP_SERVERS = "localhost:29092"
 
-with EventSource(url, headers=headers) as stream:
+# EventStream
+EVENTSTREAM_URL = 'https://stream.wikimedia.org/v2/stream/recentchange'
+EVENTSTREAM_HEADER = {"User-Agent": "Wikimedia-Analytics/0.1 nacht29.study@gmail.com"}
+KAFKA_TOPIC = "wikimedia.test.raw"
+
+# Kafka bootstrap server config
+config = {
+	"bootstrap.servers": BOOTSTRAP_SERVERS,
+	"client.id": socket.gethostname()
+}
+
+with EventSource(url=EVENTSTREAM_URL, headers=EVENTSTREAM_HEADER) as stream:
+	key = 0
 	for event in stream:
-		with open("event-payload.json", 'w', encoding='utf-8') as event_file:
-			event_file.write(json.loads(event.data))
 		if event.type == 'message':
 			try:
 				change = json.loads(event.data)
-			except ValueError:
-				pass
+			except ValueError as error:
+				print(
+					f"Failed to load data: {EVENTSTREAM_URL}"
+					f"Error: {error}"
+				)
 			else:
 				if change['meta']['domain'] == 'canary':
 					continue
-				print(f"{change['user']} edited {change['title']}")
+
+				# create a broker instance and write to topic
+				with Producer(config) as producer:
+					value = f"{change['user']} edited {change['title']}"
+					producer.produce(topic=KAFKA_TOPIC, value=value)
+					producer.poll(0)
+					producer.flush()
+				key += 1
 
 '''
 from pywikibot.comms.eventstreams import EventStreams
