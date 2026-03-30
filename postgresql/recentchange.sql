f6dfa33d6f8d3c1953ff44742aff24d07a96b819
nacht29 <nacht29.study@gmail.com>
2026-03-31 00:53:42 +0800

    feat: Added template script to create tables and schemas for recentchange in PostgreSQL

---
+++ postgresql/recentchange.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS "wikimedia.raw_eventstream";
+CREATE SCHEMA IF NOT EXISTS "wikimedia.recentchange";
+
+/*
+CREATE TABLE IF NOT EXISTS "wikimedia.raw_eventstream.recentchange_raw" (
+	id INT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS "wikimedia.recentchange.edits" (
+	id INT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS "wikimedia.recentchange.categorise" (
+	id INT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS "wikimedia.recentchange.logs" (
+	id INT NOT NULL PRIMARY KEY
+);
+*/
--- postgresql/test-script.sql
+++ postgresql/test-script.sql
@@ -1,12 +1,33 @@
--- CREATE SCHEMA test;
+/*
+UPDATE pg_database SET datallowconn = 'false' WHERE datname IN ('postgres', 'nacht29');
 
--- CREATE TABLE test.test_table(
--- 	test_col1 INT PRIMARY KEY,
--- 	test_col2 VARCHAR(10)
--- );
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname IN ('postgres', 'nacht29');
 
--- INSERT INTO test.test_table
--- VALUES
--- 	(1, 'c1');
+DROP DATABASE IF EXISTS postgres;
+DROP DATABASE IF EXISTS nacht29;
+*/
 
-SELECT * FROM test.test_table
+/*  -- for psql shell
+SELECT 'CREATE DATABASE wikimedia'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'wikimedia')\gexec
+*/
+
+-- CREATE DATABASE wikimedia;
+
+CREATE SCHEMA IF NOT EXISTS "wikimedia.test";
+
+CREATE TABLE IF NOT EXISTS "wikimedia.test.test_table"(
+	test_col1 INT PRIMARY KEY,
+	test_col2 VARCHAR(10)
+);
+
+
+INSERT INTO "wikimedia.test.test_table"
+VALUES
+	(1, 'c1'),
+	(2, 'c2')
+ON CONFLICT (test_col1) DO NOTHING;;
+
+SELECT * FROM "wikimedia.test.test_table";
